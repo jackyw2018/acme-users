e63f590ec335939a747bff99d13717f4f0d14af8
Jacky Wong <jackyw2017@gmail.com>
2019-04-23 07:45:27 -0400

    highlight feature complete

--- src/Components/Users/Highlight.js
+++ src/Components/Users/Highlight.js
@@ -1,27 +1,55 @@
 import React, { Fragment } from 'react';
 
 const Highlight = ({ text = '', highlight = '' }) => {
+  // styling for highlighted texts
   const styles = {
     highlight: {
       backgroundColor: '#FFD770',
     },
   };
 
-  const parts = (str, length) => {
-    return str.match(new RegExp('.{1,' + length + '}', 'g'));
-  };
+  // helper function that turns a string to [{text: 'aa', toHighlight: false}...]
+  const breakTextToObjects = (text, highlight) => {
+    let arrOfObjects = [];
 
-  if (!highlight) return <Fragment>{text}</Fragment>;
+    // if text does not include the text to highlight or text to highlight does not exist
+    // return [{text:'inputText', toHighlight: false}]
+    if (!text.toLowerCase().includes(highlight.toLowerCase()) || !highlight) {
+      arrOfObjects.push({ text, toHighlight: false });
+      return arrOfObjects;
+    }
+
+    let i = 0;
+
+    // loop through and keep track
+    while (i < text.length) {
+      const subStr = text.slice(i, i + highlight.length);
+
+      if (subStr.toLowerCase() === highlight.toLowerCase()) {
+        arrOfObjects.push({ text: subStr, toHighlight: true });
+        i = i + highlight.length;
+      } else {
+        if (!arrOfObjects.length)
+          arrOfObjects.push({ text: text[i], toHighlight: false });
+        else if (!arrOfObjects[arrOfObjects.length - 1].toHighlight) {
+          arrOfObjects[arrOfObjects.length - 1].text += text[i];
+        } else {
+          arrOfObjects.push({ text: text[i], toHighlight: false });
+        }
+        i++;
+      }
+    }
+
+    return arrOfObjects;
+  };
 
   return (
     <Fragment>
-      {parts(text, highlight.length).map((subStr, idx) => {
-        return subStr.toLowerCase() !== highlight.toLowerCase() ? (
-          subStr
+      {breakTextToObjects(text, highlight).map(({ text, toHighlight }) => {
+        return toHighlight ? (
+          <span style={styles.highlight}>{text}</span>
         ) : (
-          <span key={idx} style={styles.highlight}>
-            {subStr}
-          </span>
+          text
         );
       })}
     </Fragment>
